51a3b8d7090c3921a9dbc4847812752c7f317282
Nolan Picini <nolanpic@gmail.com>
2025-06-17 16:07:20 -0700

    fix skeleton animation

--- app/components/FeedSkeleton.tsx
+++ app/components/FeedSkeleton.tsx
@@ -6,15 +6,25 @@ export default function FeedSkeleton() {
       {[...Array(6)].map((_, i) => (
         <article key={i} className={styles.skeletonPostWrapper}>
           <div className={styles.skeletonFeedPost}>
-            <div className={styles.skeletonAvatar} />
+            <div className={`${styles.skeletonAvatar} ${styles.skeleton}`} />
             <div className={styles.skeletonPostRight}>
               <div className={styles.skeletonPostInfo}>
-                <div className={styles.skeletonAuthorName} />
-                <div className={styles.skeletonTimestamp} />
-                <div className={styles.skeletonMessageThread} />
+                <div
+                  className={`${styles.skeletonAuthorName} ${styles.skeleton}`}
+                />
+                <div
+                  className={`${styles.skeletonTimestamp} ${styles.skeleton}`}
+                />
+                <div
+                  className={`${styles.skeletonMessageThread} ${styles.skeleton}`}
+                />
               </div>
-              <div className={styles.skeletonContentLine1} />
-              <div className={styles.skeletonContentLine2} />
+              <div
+                className={`${styles.skeletonContentLine1} ${styles.skeleton}`}
+              />
+              <div
+                className={`${styles.skeletonContentLine2} ${styles.skeleton}`}
+              />
             </div>
           </div>
         </article>
