25c578fd6eb849e3f10180872e7269c641c230a8
Nolan Picini <nolanpic@gmail.com>
2025-06-18 14:07:04 -0700

    style feed selector closed state

--- app/components/FeedSelector.tsx
+++ app/components/FeedSelector.tsx
@@ -3,7 +3,12 @@ import styles from "./FeedSelector.module.css";
 export default function FeedSelector() {
   return (
     <nav className={styles.feedSelector}>
-      <span>All feeds</span>
+      <span className={styles.feedSelectorText}>All feeds</span>
+      <img
+        className={styles.dropdownArrow}
+        src="/icons/dropdown-arrow.svg"
+        alt="Expand feed selector"
+      />
     </nav>
   );
 }
