8d05fd2a6b882739e4db28a9bdd12eaff9ad9483
Nolan Picini <nolanpic@gmail.com>
2025-08-15 18:46:30 -0700

    fix incorrect button export

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/app/components/common/Button";
+import Button from "@/app/components/common/Button";
 import { Input } from "@/app/components/common/Input";
 import styles from "./page.module.css";
 import { useQuery } from "convex/react";
