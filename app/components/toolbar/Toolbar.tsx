7b24d959d204c4bba8320c66b41befc132f2a3d7
Nolan Picini <nolanpic@gmail.com>
2025-09-24 11:20:23 -0700

    only show post toolbar buttons when signed in

--- app/components/toolbar/Toolbar.tsx
+++ app/components/toolbar/Toolbar.tsx
@@ -1,31 +1,38 @@
 import styles from "./Toolbar.module.css";
 import IconButton from "../common/IconButton";
+import { useAuthedUser } from "@/app/hooks/useAuthedUser";
 
 export default function Toolbar() {
+  const { isSignedIn } = useAuthedUser();
+
   return (
     <div className={styles.toolbar}>
-      <IconButton
-        icon="ellipsis"
-        label="More!"
-        className={styles.overflowMenuButton}
-      />
-      <IconButton
-        icon="pen"
-        variant="primary"
-        label="New post"
-        className={styles.newPostButton}
-      />
-      <IconButton
-        icon="toggles"
-        label="Feed settings"
-        className={styles.feedSettingsButton}
-      />
-      <IconButton
-        icon="gear"
-        label="Admin"
-        className={styles.adminSettingsButton}
-        iconSize={32}
-      />
+      {isSignedIn && (
+        <>
+          <IconButton
+            icon="ellipsis"
+            label="More!"
+            className={styles.overflowMenuButton}
+          />
+          <IconButton
+            icon="pen"
+            variant="primary"
+            label="New post"
+            className={styles.newPostButton}
+          />
+          <IconButton
+            icon="toggles"
+            label="Feed settings"
+            className={styles.feedSettingsButton}
+          />
+          <IconButton
+            icon="gear"
+            label="Admin"
+            className={styles.adminSettingsButton}
+            iconSize={32}
+          />
+        </>
+      )}
     </div>
   );
 }
