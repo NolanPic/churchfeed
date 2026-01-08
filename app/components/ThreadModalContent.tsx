9c88762853de42a5f8c4f0633fe68ae642f16ad5
Nolan Picini <nolanpic@gmail.com>
2026-01-08 14:47:10 -0800

    unset feed id on thread modal unmount

--- app/components/ThreadModalContent.tsx
+++ app/components/ThreadModalContent.tsx
@@ -28,6 +28,8 @@ export default function ThreadModalContent({
 
   useEffect(() => {
     setFeedIdOfCurrentThread(feedId);
+
+    return () => setFeedIdOfCurrentThread(undefined);
   }, [feedId, setFeedIdOfCurrentThread]);
 
   if (!thread) {
